4945b7c6de0b9f010db8fbbc1f270ba4053bd1c6
cookie-army <cookiearmycia@gmail.com>
2023-09-13 17:19:01 +0530

    Spring Boot CRUD Operations

---
+++ src/main/java/com/example/BasicSpringBootApp/BasicSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.BasicSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootAppApplication.class, args);
+	}
+}
